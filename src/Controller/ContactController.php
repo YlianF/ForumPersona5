5161195bd456a7402aef38025adca9e7d81c85c0
ylian <ylian.fatmi@etu.unilim.fr>
2022-11-11 12:43:48 +0100

    ajout entité contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Repository\ContactRepository;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\ContactType;
 use App\Form\EmailType;
@@ -12,23 +14,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request): Response
+    public function index(Request $request, ContactRepository $contactRepository): Response
     {
         $form=$this->createForm(ContactType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $data=$form->getData();
-            $email=$data['email'];
 
-            return $this->render('contact/success.html.twig', [
-                'email' => $email
-            ]);
+            $contact = new Contact();
+            $contact->setAuteur($this->getUser());
+            $contact->setMessage($data['message']);
+
+
+            $contactRepository->save($contact, true);
+
+            return $this->render('contact/success.html.twig');
+
         } else {
             return $this->renderForm('contact/index.html.twig', [
                 'controller_name' => 'ContactController',
                 'formulaire' => $form
             ]);
+
         }
 
         
---
+++ src/Entity/Contact.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ContactRepository::class)]
+class Contact
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $auteur = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $message = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?User
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(?User $auteur): self
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+}
